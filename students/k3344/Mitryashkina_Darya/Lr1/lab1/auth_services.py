76a6406db6cc3850da0db93e557f2f1e2de049dc
Darya <104630608+daryaami@users.noreply.github.com>
2025-05-29 09:23:31 +0300

    refactor

--- students/k3344/Mitryashkina_Darya/Lr1/lab1/auth_services.py
+++ students/k3344/Mitryashkina_Darya/Lr1/lab1/auth_services.py
@@ -13,9 +13,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = "your_secret_key_here"  
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30  
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES") 
 
 from fastapi.security import OAuth2PasswordBearer
 
